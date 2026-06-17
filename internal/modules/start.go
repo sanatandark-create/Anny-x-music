1870e9e274cc030ddc9f9098969b1081c3504e16
sanatandark-create <sanatandark@gmail.com>
2026-06-17 13:26:29 +0530

    Update start.go

--- internal/modules/start.go
+++ internal/modules/start.go
@@ -1,12 +1,18 @@
 /*
- * ○ A high-performance engine for streaming music in Telegram voicechats.
+ * ○ Anny X Music - A high-performance engine for streaming music in Telegram voicechats.
  *
- * Copyright (C) 2026 Team Arc
+ * Copyright (C) 2026 @Mad_x_Avi
+ * All rights reserved.
+ *
+ * This source code is proprietary and confidential.
+ * Unauthorized copying, distribution, or modification is strictly prohibited.
  */
 
 package modules
 
 import (
+	"time"
+
 	"github.com/Laky-64/gologging"
 	tg "github.com/amarnathcjd/gogram/telegram"
 
@@ -18,7 +24,7 @@ import (
 )
 
 func init() {
-	helpTexts["/start"] = `<i>Start the bot and show main menu.</i>`
+	helpTexts["/start"] = `<i>Start Anny X Music and show main menu.</i>`
 }
 
 func startHandler(m *tg.NewMessage) error {
@@ -47,12 +53,65 @@ func startHandler(m *tg.NewMessage) error {
 		helpHandler(m)
 
 	default:
+		// Send heart reaction to the /start message
+		_, err := m.React(tg.ReactionEmoji{
+			Emoticon: "❤️",
+		})
+		if err != nil {
+			gologging.Error("[start] Failed to send heart reaction: " + err.Error())
+		}
+
+		// Send animated welcome sequence
+		animations := []string{
+			"✨ <b>Welcome to</b> ✨",
+			"🎵 <b>Anny X Music</b> 🎵",
+			"🚀 <b>The Ultimate Streaming Bot</b> 🚀",
+			"🎧 <b>Powered by @Mad_x_Avi</b> 🎧",
+		}
+
+		for i, animText := range animations {
+			var msg *tg.NewMessage
+			if i == 0 {
+				msg, err = m.Reply(animText)
+			} else {
+				msg, err = m.Client.EditMessage(m.ChatID(), msg.ID, animText)
+			}
+			if err != nil {
+				gologging.Error("[start] Animation edit failed: " + err.Error())
+			} else {
+				time.Sleep(800 * time.Millisecond)
+			}
+		}
+
+		// Delete the animation messages
+		m.Client.DeleteMessages(m.ChatID(), []int32{msg.ID})
+
+		// Send sticker with auto-deletion after 3 seconds
+		stickerID := "CAACAgEAAxkBAAERZ0NqMlGDnVBT_h1vm1qbL3Fe8_qjigACVAYAAlmWeUd1rCk8DBvZdjwE"
+		stickerMsg, err := m.Client.SendMessage(m.ChatID(), &tg.MessageMedia{
+			Document: &tg.Document{
+				ID: stickerID,
+			},
+		})
+		if err != nil {
+			gologging.Error("[start] Failed to send sticker: " + err.Error())
+		} else {
+			// Delete sticker after 3 seconds
+			go func() {
+				time.Sleep(3 * time.Second)
+				m.Client.DeleteMessages(m.ChatID(), []int32{stickerMsg.ID})
+			}()
+		}
+
+		// Brief pause before sending main menu
+		time.Sleep(500 * time.Millisecond)
+
 		caption := F(m.ChannelID(), "start_private", locales.Arg{
 			"user": utils.MentionHTML(m.Sender),
 			"bot":  utils.MentionHTML(m.Client.Me()),
 		})
 
-		_, err := m.RespondMedia(&tg.InputMediaWebPage{
+		_, err = m.RespondMedia(&tg.InputMediaWebPage{
 			URL:             config.StartImage,
 			ForceLargeMedia: true,
 		}, &tg.MediaOptions{
